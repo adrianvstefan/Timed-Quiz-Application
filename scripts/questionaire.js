f4348b4eb1755ede8c8f869d9b5f61325c8b519d
silestefan <147498570+silestefan@users.noreply.github.com>
2024-01-14 10:00:25 +0000

    Added questions for the quiz

---
+++ scripts/questionaire.js
@@ -0,0 +1,52 @@
+var questionaire = [
+    {
+        question: "In what franchise would you find the character Katniss Everdeen?",
+        choiseArray: ["Game of Thrones", "The Hobbit", "The Hunger Games", "Harry Potter"],
+        answer: "The Hunger Games"
+    },
+    {
+        question: "What does Hakuna Matata mean?",
+        choiseArray: ["Go away", "No worries", "Life's great", "Look close"],
+        answer: "No worries"
+    },
+    {
+        question: "Who painted the Mona Lisa?",
+        choiseArray: ["Leonardo da Vinci", "Michelangelo", "Raphael", "Donatello"],
+        answer: "Leonardo da Vinci"
+    },
+    {
+        question: "Which planet is closest to the sun?",
+        choiseArray: ["Venus", "Mars", "Earth", "Mercury"],
+        answer: "Mercury"
+    },
+    {
+        question: "What is the largest country in the world?",
+        choiseArray: ["USA", "Australia", "Russia", "China"],
+        answer: "Russia"
+    },
+    {
+        question: "What is the capital of Iceland?",
+        choiseArray: ["Stockholm", "Reykjavík", "Oslo", "Tallinn"],
+        answer: "Reykjavík"
+    },
+    {
+        question: "What was Queen Elizabeth II's surname?",
+        choiseArray: ["Windsor", "Mountbatten", "Saxe-Coburg-Gotha", "Spencer"],
+        answer: "Reykjavík"
+    },
+    {
+        question: "What is the name of a shape with nine sides?",
+        choiseArray: ["Decagon", "Heptagon", "Hexagon", "Nonagon"],
+        answer: "Nonagon"
+    },
+    {
+        question: "Where is known as 'the happiest place on Earth?'",
+        choiseArray: ["Switzerland", "Luxembourg", "Disneyland", "Finland"],
+        answer: "Disneyland"
+    },
+    {
+        question: "What is the name of a shape with nine sides?",
+        choiseArray: ["Sc", "Sn", "Fe", "Mn"],
+        answer: "Sn"
+    }
+]
